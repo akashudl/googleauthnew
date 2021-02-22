6b08de1fa797588fadc779bcd0ffea7b484daffa
akashudl <78961715+akashudl@users.noreply.github.com>
2021-02-22 09:28:30 +0530

    Add files via upload

---
+++ src/Signup.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import StyleFirebaseUi from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+class Signup extends Component
+{
+    render ()
+    {
+        return (
+            <div>
+                <h2>
+                    Hello User  Welcome
+                </h2>
+            </div>
+ 
+        );
+    }
+}
+export default Signup;
